80423b9df37990187a5107966a397b7a441105d5
TitoGod <mauro.dr07@gmail.com>
2023-07-27 22:36:28 -0300

    actualizado de nuevo

--- main.py
+++ main.py
@@ -3,4 +3,9 @@
 import requests
 import pandas as pd
 from config import password
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+
 
