88d7d77c0e5dab9fd7a13197999f5127edc71b24
TitoGod <mauro.dr07@gmail.com>
2023-08-22 20:07:53 -0300

    funciona bien

--- main.py
+++ main.py
@@ -11,22 +11,22 @@ coordList = ["lat=51.5085&lon=-0.1257", "lat=40.7143&lon=-74.006", "lat=-31.4135
 if __name__ == "__main__":
     # 1: Traer todos los datos de la API
     get_weatherdata(cityList, coordList)
-    time.sleep(5)
+    time.sleep(2)
 
     # 2: Listar las rutas de los archivos JSON para sacar los datos
     lista_de_rutas = listar_rutas('weather_data_')
-    time.sleep(5)
+    time.sleep(2)
 
     # 3: Transformar todos esos datos y dejarlos en una lista que contenga todos los DataFrame
     lista_de_dfs = []
     for ruta in lista_de_rutas:
         dfs = transform(ruta)
         lista_de_dfs.append(dfs)
-    time.sleep(5)
+    time.sleep(2)
 
     # 3: Crear la estructura de la tabla en la base de datos
     create_table()
-    time.sleep(5)
+    time.sleep(2)
 
     # 4: Agregar todos los datos de los DataFrame a la base de datos
     agregar_datos(lista_de_dfs)
